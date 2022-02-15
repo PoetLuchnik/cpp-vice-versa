3c0dbd03222b0dac1dd7f4893cd241e4d1f6f040
PoetLuchnik <artem.pirunow@gmail.com>
2022-02-15 15:29:12 +0200

    cctype implementation

---
+++ cctype.h
@@ -0,0 +1,16 @@
+#pragma once
+
+int cvv_isalnum(int c);
+int cvv_isalpha(int c);
+int cvv_isblank(int c);
+int cvv_iscntrl(int c);
+int cvv_isdigit(int c);
+int cvv_isgraph(int c);
+int cvv_islower(int c);
+int cvv_isprint(int c);
+int cvv_ispunct(int c);
+int cvv_isspace(int c);
+int cvv_isupper(int c);
+int cvv_isxdigit(int c);
+int cvv_tolower(int c);
+int cvv_toupper(int c);
